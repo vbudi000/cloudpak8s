518ffd5c83ee6d2c03ea8ad33d9a6b5fb280048a
John Webb <john@johns-macbook.raleigh.ibm.com>
2020-01-23 15:20:05 -0500

    Reordered sections and cleanup old asetts.

--- assets/cp4mcm/scripts/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-helm install . --name mq101 --namespace mq --tls
-helm list mq101 --tls
-oc get Pods -n mq --show-labels
-oc get StatefulSet -n mq --show-labels
-oc get Service -n mq --show-labels
